b96c33258008ab6aaaa569bb7dc79925ced37009
Com Home <Com_Home@example.com>
2015-12-02 23:44:40 +0700

    New version

--- MySql/lab_nodejs01.sql
+++ MySql/lab_nodejs01.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2015 at 07:59 PM
+-- Generation Time: Dec 02, 2015 at 05:42 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,24 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+`category_id` int(10) NOT NULL,
+  `category_name` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Technology'),
+(2, 'Education'),
+(3, 'World');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `topic`
 --
 
 CREATE TABLE IF NOT EXISTS `topic` (
 `id_topic` int(10) NOT NULL,
-  `id_user` int(10) NOT NULL,
   `title_topic` text NOT NULL,
   `des_topic` text NOT NULL,
+  `category_id` int(10) NOT NULL,
   `pic_topic` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `topic`
---
-
-INSERT INTO `topic` (`id_topic`, `id_user`, `title_topic`, `des_topic`, `pic_topic`) VALUES
-(86, 0, 'How a 3-D-printer changed a 4-year-old''s heart and life', '-แมวตายอนาต', '1310151254201020666.jpg'),
-(89, 0, 'How a 3-D-printer changed a 4-year-old''s heart and life', 'Earlier this year, the hospital got a 3-D printer that makes exact replicas of organs that doctors can use to plan surgery, and even do practice operations. The printer uses images from patients'' MRI or CT scan images as a template and lays down layers of rubber or plastic.\r\n\r\nDr. Redmond Burke, director of pediatric cardiovascular surgery at Nicklaus Children''s Hospital, meditated on the model of Mia''s heart for a couple of weeks. He showed it to colleagues for their input and even carried it around in his gym bag for quick reference.\r\n\r\nBurke finally had the "Aha!" insight. Instead of making an incision on the left side for this type of heart defect, called double aortic arch, he should cut into Mia''s chest from the right.', '1310151216391039698.jpg');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -59,14 +71,18 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id_user`, `user_name`, `user_pass`) VALUES
-(1, 'sumate', 'dfsdfsdf@hotmail.com'),
-(2, 'sdfsdfs', 'aaa3tg'),
 (3, 'Hello', 'content Hello'),
 (4, 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+ ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `topic`
@@ -85,10 +101,15 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+MODIFY `category_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `topic`
 --
 ALTER TABLE `topic`
-MODIFY `id_topic` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=90;
+MODIFY `id_topic` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
